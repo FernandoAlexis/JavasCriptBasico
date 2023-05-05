598e0abaa49807fb80858be1b7ae2781802aaa25
FernandoAlexis <62911105+FernandoAlexis@users.noreply.github.com>
2023-05-05 05:25:36 -0600

    Firts commit

---
+++ main.js
@@ -0,0 +1,60 @@
+
+//let nombre ="Legolas";
+
+//let edad= 23;
+
+
+//et concatencacion= nombre + "" + edad;
+
+
+/*let datos = document.getElementById("datos");
+
+datos.innerHTML=`<h1>Soy el div de los datos</h1>
+                 <h2> Mi nombre es: ${nombre}</h2>
+                 <h2> Mi edad es: ${edad}</h2>`;
+
+
+if(edad >=18){
+    datos.innerHTML += '<h1> Eres mayor de edad </h1>';
+
+}else{
+    datos.innerHTML += '<h2> Eres menor de edad </h2>';
+}
+
+
+for(let i=0; i<100; i++){
+    datos.innerHTML += "<h2> Estamos en el año:"+i;
+}*/
+
+
+/*function MostrarNombre(nombre,edad){
+    let datos = `<h1>Soy el div de los datos</h1>
+    <h2> Mi nombre es: ${nombre}</h2>
+    <h2> Mi edad es: ${edad}</h2>`;
+
+    return datos;
+}
+
+
+
+function imprimir(){
+    let datos= document.getElementById("datos");
+    datos.innerHTML=MostrarNombre(nombre,edad);
+}
+
+
+imprimir();
+
+let nombres= ['Legolas','Aragon','Gandalf'];
+
+
+nombres.forEach(function(nombres){
+
+    document.write(nombres+ '<br/>');
+})*/
+
+
+
+
+
+
